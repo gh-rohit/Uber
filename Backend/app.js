1828ea7ee897a6f20df08e80fe998cf2148f35f5
rohitrajcse00@gmail.com <rohitrajcse00@gmail.com>
2024-12-01 15:31:00 +0530

    auth controller

--- Backend/app.js
+++ Backend/app.js
@@ -3,9 +3,11 @@ dotenv.config()
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const cookieParser = require('cookie-parser');
 const connectToDb = require('./db/db')
 connectToDb()
 app.use(cors())
+app.use(cookieParser())
 const userRoutes = require('./routes/user-route')
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
